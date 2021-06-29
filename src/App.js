e0d47f11fa2c534fed99b2f9927a58bda6816d78
Federico Depetris <depetrisfe@gmail.com>
2021-06-28 22:19:21 -0300

    sidebar ahora se despliega correctamente

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Header from "./Header";
 import FrontPageWeapons from "./Components/Weapons/FrontPageWeapons";
-//import Sidebar from "./Components/Sidebar/Sidebar";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
     <>
       <Header />
       <div className="content">
+        <Sidebar />
         <FrontPageWeapons />
       </div>
     </>
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./sidebar.css";
 
 const Sidebar = () => {
+  const [toggled, setToggled] = useState("active");
+
+  const onClickHandler = () => {
+    if (toggled === "active") {
+      setToggled("inactive");
+    } else {
+      setToggled("active");
+    }
+  };
+
   return (
     <div>
-      <div id="leftMenu">
-        <ul>
-          <li>Weapons</li>
-          <li>Agents</li>
-          <li>Maps</li>
-        </ul>
+      <div id="leftMenu" className={toggled}>
+        <div className="toggle-btn" onClick={() => onClickHandler()}>
+          <span>&#9776;</span>
+        </div>
+        <ul></ul>
       </div>
     </div>
   );
