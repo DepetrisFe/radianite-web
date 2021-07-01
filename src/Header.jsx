6ab0f0344c2d633077c92352c6a17e2df9294223
Federico Depetris <depetrisfe@gmail.com>
2021-07-01 14:33:41 -0300

    Logo agregado

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <div className="header"></div>;
+  return (
+    <>
+      <div className="headerStyle">
+        <img src="https://i.postimg.cc/4xpVmsFS/radianite3521.png" alt="Logo" />
+      </div>
+    </>
+  );
 };
 
 export default Header;
