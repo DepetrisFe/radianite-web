12aa89a264fff49cf2e4b6696e28219fab8dba54
Federico Depetris <depetrisfe@gmail.com>
2021-07-06 20:41:51 -0300

    componente agentes creado

--- src/Components/Agents/FrontPageAgents.jsx
+++ src/Components/Agents/FrontPageAgents.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./frontPageAgents.css";
 
 const FrontPageAgents = () => {
+  //useState que rellena el array de weapons
+  const [agents, setAgents] = useState([]);
+  const baseUrl = "https://valorant-api.com/v1/agents";
+
+  const getAgents = async () => {
+    try {
+      const result = await fetch(baseUrl);
+      const finalResult = await result.json();
+      setAgents(finalResult.data);
+    } catch (e) {
+      console.log("error", e);
+    }
+  };
+
+  useEffect(() => {
+    getAgents();
+  }, []);
+
+  console.log(agents);
+
   return (
-    <>
-      <div className="agentsMain">Sección Agentes en contrucción</div>
-    </>
+    <div className="agentsMain">
+      {agents.map((item) => (
+        <div className="agent" key={item.uuid}>
+          <img src={item.displayIcon} alt={item.displayName} />
+          <p>{item.displayName}</p>
+        </div>
+      ))}
+    </div>
   );
 };
 
