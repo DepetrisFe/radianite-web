197ecba59af1580c3c21c2eb5830aeead47d93ad
Federico Depetris <depetrisfe@gmail.com>
2021-07-18 17:44:48 -0300

    esta es la segunda prueba de git

--- src/Components/Maps/FrontPageMaps.jsx
+++ src/Components/Maps/FrontPageMaps.jsx
@@ -37,6 +37,7 @@ const FrontPageMaps = () => {
   }
   return (
     <div className="mapsMain">
+      <p>Esta es una segunda prueba para git</p>
       {maps.map((item) => (
         <div key={item.uuid}>
           <Link className="map" to={`/maps/${item.uuid}`}>
