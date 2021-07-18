99a7c7a4b4fab8d72036bf9e2e00917fc032a5b4
depetrisfe <depetrisfe@gmail.com>
2021-07-18 20:19:39 -0300

    test desde la pc de escritorio

--- src/Components/Maps/FrontPageMaps.jsx
+++ src/Components/Maps/FrontPageMaps.jsx
@@ -38,8 +38,7 @@ const FrontPageMaps = () => {
   return (
     <div className="mapsMain">
       <p>Esto es una prueba para git</p>
-      <p>Esto es branch 1</p>
-      <p>Esto es branch 2</p>
+      <p>esto es una prueba desde la pc de escritorio</p>
       {maps.map((item) => (
         <div key={item.uuid}>
           <Link className="map" to={`/maps/${item.uuid}`}>
