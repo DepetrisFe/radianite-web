98053e7d962b11051d89bd81ff2fe9206ca561c3
Federico <depetrisfe@gmail.com>
2021-08-25 21:43:20 -0300

    comentario removido

--- src/Components/Maps/FrontPageMaps.jsx
+++ src/Components/Maps/FrontPageMaps.jsx
@@ -10,7 +10,6 @@ const FrontPageMaps = () => {
   const [hasError, setHasError] = useState(false);
   const baseUrl = "https://valorant-api.com/v1/maps";
 
-  //comentario para controlar que funcione git, luego borrar
   useEffect(() => {
     async function fetchData() {
       try {
